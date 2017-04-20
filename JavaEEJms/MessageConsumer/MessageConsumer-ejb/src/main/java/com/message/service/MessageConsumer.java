7f879595cc0ed7f42887464c89fd3a9137a1c4a0
Oleksandr Serogin <oserogin@luxoft.com>
2017-04-20 12:54:10 +0300

    JavaEE MessageConsumer: initial version

---
+++ JavaEEJms/MessageConsumer/MessageConsumer-ejb/src/main/java/com/message/service/MessageConsumer.java
@@ -0,0 +1,27 @@
+package com.message.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * @author Oleksandr Serogin
+ */
+@Slf4j
+@MessageDriven(mappedName = "jms/feedTopic", name = "MessageHandler")
+public class MessageConsumer implements MessageListener {
+    @Override
+    public void onMessage(final Message msg) {
+        try {
+            TextMessage message = (TextMessage) msg;
+            log.info("Message (id = {}) is received with next content: {}", message.getJMSMessageID(), message.getText());
+        } catch (JMSException ex) {
+            log.error("Error getting message {}", msg, ex);
+        }
+    }
+}
+
