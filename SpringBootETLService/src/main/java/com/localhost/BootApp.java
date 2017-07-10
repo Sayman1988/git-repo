4fc1d8f9cf5cc9980e7068b9932bd6ac3f22addf
Sayman1988 <aleks.seregin@gmail.com>
2017-07-10 23:01:27 +0300

    SpringBootETLService initial working version

---
+++ SpringBootETLService/src/main/java/com/localhost/BootApp.java
@@ -0,0 +1,14 @@
+package com.localhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Admin on 10.07.2017.
+ */
+@SpringBootApplication
+public class BootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApp.class, args);
+    }
+}
