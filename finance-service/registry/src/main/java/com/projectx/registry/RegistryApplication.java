a50a706b7dabd98187a65b7ed90686870608b494
o.serohin <elevator2mac22>
2017-12-15 14:35:25 +0200

    Up external Configuration Server with Registry Service configuration

--- finance-service/registry/src/main/java/com/projectx/registry/RegistryApplication.java
+++ finance-service/registry/src/main/java/com/projectx/registry/RegistryApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
 /**
  *
  * @author Oleksandr Serohin
@@ -12,7 +11,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class RegistryApplication {
-    
     public static void main(String[] args) {
         SpringApplication.run(RegistryApplication.class, args);
     }
